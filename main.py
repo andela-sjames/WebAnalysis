51a964287d6aaea8e0a5d0a489cfa6b69605b5bc
andela-sjames <samuel.james@andela.com>
2019-06-02 23:03:08 +0100

    More pep8 on main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from dataclasses import dataclass
 from urllib import request, error, parse
 
+
 @dataclass
 class DeepWebAnalyzer:
     root: str
@@ -20,7 +21,7 @@ class DeepWebAnalyzer:
         external_links = self.get_external(page1, self.root)
         crawled = {}
         crawldepth = {}
-        crawled[page_title] = {'parent':'root'}
+        crawled[page_title] = {'parent': 'root'}
         num_of = len(external_links)
         print(f" *** {num_of} external links found on root")
         for ext_link in external_links:
@@ -30,41 +31,44 @@ class DeepWebAnalyzer:
                 continue
 
             filter_domain = [domain]
-            #set domain and depth
-            tocrawl = [[ext_link, 1]]
+            tocrawl = [[ext_link, 1]]  # set domain and depth
 
-            while tocrawl: 
+            while tocrawl:
                 crawl_element = tocrawl.pop()
                 link = crawl_element[0]
                 depth = crawl_element[1]
-                
+
                 if link not in crawled.keys():
-                    if link is not None and link[:1] != '#' and link[:1] != '/' and \
-                        link[:1] != '?' and link[:2] != '//':
+                    if link is not None and link[:1] != '#' and \
+                       link[:1] != '/' and link[:1] != '?' and \
+                       link[:2] != '//':
                         print("*** Fetching data from " + link)
                     content, title = self.get_page(link)
 
                     if title is type(str):
                         title = re.sub("[!@#$']", '', title)
-                    
+
                     if content is None:
                         continue
                     else:
                         crawldepth[depth] = title
                     host = self.get_domain(link)
-                    
+
                     if depth < self.max_depth and host in filter_domain:
 
                         outlinks = self.get_all_links(content, link)
                         num_of_outlinks = len(outlinks)
-                        print(f" *** {num_of_outlinks} link(s) found on {link}")
-                    
-                        self.add_to_tocrawl(crawled.keys(), tocrawl, outlinks, depth + 1)
-                    
+                        print(f"*** {num_of_outlinks} link(s) found on {link}")
+
+                        self.add_to_tocrawl(
+                            crawled.keys(), tocrawl, 
+                            outlinks, depth + 1
+                        )
+
                     if depth == 1:
-                        crawled[title] = {'parent':page_title}
+                        crawled[title] = {'parent': page_title}
                     else:
-                        crawled[title] = {'parent':crawldepth[depth - 1]}
+                        crawled[title] = {'parent': crawldepth[depth - 1]}
 
         return crawled
     
@@ -80,9 +84,9 @@ class DeepWebAnalyzer:
 
         host = parse.urlparse(host).hostname
         hostname = parse.urlparse(root).hostname
-        if host == None:
+        if host is None:
             return False
-        return host != hostname and host.find(hostname) == -1
+        return host is not hostname and host.find(hostname) == -1
 
     def get_external(self, soup, url):
         return [l.get('href') for l in soup.findAll('a') if self.is_external(url, l.get('href'))]
@@ -90,8 +94,8 @@ class DeepWebAnalyzer:
     def get_domain(self, url):
         
         hostname = parse.urlparse(url).hostname
-        
-        if len(re.findall( r'[0-9]+(?:\.[0-9]+){3}', hostname)) > 0:
+
+        if len(re.findall(r'[0-9]+(?:\.[0-9]+){3}', hostname)) > 0:
             return hostname
         elif len(hostname.split('.')) == 0:
             hostname
@@ -120,7 +124,7 @@ class DeepWebAnalyzer:
             return None, None
 
     def get_all_links(self, page, parent):
-        return [l.get('href') for l in page.findAll('a') ]
+        return [l.get('href') for l in page.findAll('a')]
 
 
 dwa = DeepWebAnalyzer("https://www.cnn.com", 1)
